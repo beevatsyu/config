6e922e25d9b5407059f30da2f8813e42d6b1b343
Partha Bera <partha.bera@afourtech.com>
2016-12-20 12:32:36 +0530

    backup old files before symlink

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 #! /bin/bash
 # source this file
+if [ -f ~/.bashrc ]; then
+	cp ~/.bashrc ~/.bashrc.old
+fi
 ln -sf `pwd`/.bashrc ~
+
+if [ -f ~/.vimrc ]; then
+	cp ~/.vimrc ~/.vimrc.old
+fi
 ln -sf `pwd`/.vimrc ~
+
+if [ -d ~/.vim ]; then
+	cp -r ~/.vim ~/.vim.old
+fi
 ln -sf `pwd`/.vim ~
+
 vim +PlugInstall +qa
 source ~/.bashrc
